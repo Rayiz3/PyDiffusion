f352781520596eaece30d2856a764d8d8d595607
Rayiz3 <jusmint3@gmail.com>
2023-05-24 11:30:16 +0900

    modify : gpu device managing

--- Celebahq.py
+++ Celebahq.py
@@ -1,4 +1,4 @@
-import functools
+import PIL
 import numpy as np
 
 import torch
@@ -9,39 +9,38 @@ from torchvision.datasets import celeba
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("current device : {}".format(device))
 
-def meanflat(x):
-    return torch.mean(x, dim=list(range(1, len(x.shape))))
-
+# Celeba
+img = PIL.Image.open('./img_align_celeba/000001.jpg')
+print(img.size)
 
 class GaussianDiffusion():
-    def __init__(self, betas: np.ndarray, loss_type, dtype=np.float32):
+    def __init__(self, betas, dtype=np.float32):
         # check beta
         assert (betas > 0).all() and (betas <= 1).all()  # 0 < beta <= 1
         
         # alpha and beta
         self.alphas = 1 - betas
-        self.alphas_bar = np.cumprod(betas, 0)
-        self.alphas_bar_prev = np.append(1., alphas_bar[:-1])
+        self.alphas_bar = torch.cumprod(betas, 0)
+        self.alphas_bar_prev = torch.concat([torch.tensor([1.]).to(device), self.alphas_bar[:-1]], 0)
         self.betas = betas
-        
+    
         # operation of alpha
         self.one_minus_alphas_bar = 1 - self.alphas_bar
-        self.sqrt_alphas_bar = np.sqrt(self.alphas_bar)
-        self.sqrt_one_minus_alphas_bar = np.aqrt(1 - self.alphas_bar)
-        self.log_one_minus_alphas_bar = np.log(1 - self.alphas_bar)
-        self.sqrt_recip_alphas_bar = np.sqrt(1/self.alphas_bar)
-        self.sqrt_recip_minus_one_alphas = np.sqrt(1/self.alphas_bar - 1)
+        self.sqrt_alphas_bar = torch.sqrt(self.alphas_bar)
+        self.sqrt_one_minus_alphas_bar = torch.sqrt(1 - self.alphas_bar)
+        self.log_one_minus_alphas_bar = torch.log(1 - self.alphas_bar)
+        self.sqrt_recip_alphas_bar = torch.sqrt(1/self.alphas_bar)
+        self.sqrt_recip_minus_one_alphas = torch.sqrt(1/self.alphas_bar - 1)
         
         # coefficient of posterior
         self.post_variance = (1 - self.alphas_bar_prev) / (1 - self.alphas_bar) * self.betas
-        self.post_log_clip_variance = np.log(np.max(self.post_variance, 1e-20))  # because post_variance = 0 at first
-        self.post_mean_x0 = np.sqrt(self.alphas_bar_prev) / (1 - self.alphas_bar) * self.betas
-        self.post_mean_x1 = (1 - self.alphas_bar_prev) / (1 - self.alphas_bar) * np.sqrt(1 - self.betas)
+        self.post_log_clip_variance = torch.log(torch.maximum(self.post_variance, torch.tensor([1e-20] * len(betas)).to(device)))  # because post_variance = 0 at first
+        self.post_mean_x0 = torch.sqrt(self.alphas_bar_prev) / (1 - self.alphas_bar) * self.betas
+        self.post_mean_x1 = (1 - self.alphas_bar_prev) / (1 - self.alphas_bar) * torch.sqrt(1 - self.betas)
         
         # others
-        self.loss_type = loss_type
         self.time_steps = betas.shape
-        self.num_time_steps = int(self.time_steps)
+        self.num_time_steps = int(self.time_steps[0])
     
     @staticmethod
     ## index ## ==================================================
@@ -86,20 +85,6 @@ class GaussianDiffusion():
         
         return term_x_0 + term_noise
 
-    ## predict ## ================================================
-    # - predict x_0 from noise at step t
-    # [arguments]
-    # - x_t : data at step t
-    # - t : time step
-    # - noise : noise type
-    # ============================================================
-    def predict(self, x_t, t, noise):
-        assert x_t.shape == noise.shape
-        term_x_t = self.index(self.sqrt_recip_alphas_bar, t, x_t.shape) * x_t
-        term_noise = self.index(self.sqrt_recip_minus_one_alphas, t, x_t.shape) * noise
-        
-        return term_x_t + term_noise
-
     ## q_post_statistics ## =======================================
     # - return mean / variance / log variance of q(x_t-1 | x_t, x_0)
     # [arguments]
@@ -116,6 +101,20 @@ class GaussianDiffusion():
         log_variance = self.index(self.post_log_clip_variance, t, x_t.shape)
 
         return mean, variance, log_variance
+
+    ## predict ## ================================================
+    # - predict x_0 from noise at step t
+    # [arguments]
+    # - x_t : data at step t
+    # - t : time step
+    # - noise : noise type
+    # ============================================================
+    def predict(self, x_t, t, noise):
+        assert x_t.shape == noise.shape
+        term_x_t = self.index(self.sqrt_recip_alphas_bar, t, x_t.shape) * x_t
+        term_noise = self.index(self.sqrt_recip_minus_one_alphas, t, x_t.shape) * noise
+        
+        return term_x_t + term_noise
 
     ## p_loss ## =================================================
     # - return loss of predicted x_0
@@ -156,6 +155,18 @@ class GaussianDiffusion():
         
         return self.q_post_statistics(x_0, x_t, t)
 
-class Model(nn.Module):
-    def __init__(self, model_name, betas: np.ndarray, loss_type: str, num_classes: int, randflip, block_size: int):
-        self.model_name = model_name
+class Diffusion(nn.Module):
+    def __init__(self, time_steps, beta_start, beta_end, img_size, device='cpu'):
+        self.time_steps = time_steps
+        self.img_size = img_size
+        self.device = device
+        self.gaussian = GaussianDiffusion(torch.linspace(beta_start, beta_end, time_steps).to(device))
+    
+    def forward(self, x_0, t, noise=None):
+        B, C, H, W = x_0.shape
+        if noise == None:
+            noise = torch.randn(B, C, H, W).to(self.device)
+        
+        return self.gaussian.q_sample(x_0, t, noise=noise)
+    
+model = Diffusion(200, 10 ** -4, 0.02, (1, 178, 218), device=device)
